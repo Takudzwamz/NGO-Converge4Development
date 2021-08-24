b3839a8019fa57e83998e4297cae1b91ee9affe2
Takudzwa Mupanesure <zhout1709@hotmail.com>
2021-08-24 22:50:00 +0200

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent implements OnInit {
     this.metaTagService.addTags([
       {name: 'keywords', content: 'Converge 4 Development, C4D, NGO, None-Profit, Zimbabwe, Zimbabwe development,Sustainable, Harare, Victoria Falls, Zim, Infrastructure, Africa'},
       {name: 'robots', content: 'index, follow'},
+      {name: 'google-site-verification', content: 'zqnYEsQ6-_sH21DgGfnth3gUmjbo8bazU4lUlMBvOX8'},
       {name: 'description', content: 'C4D will reach out and call upon individuals who feel compelled by the Zimbabwean stalled development case to converge with like-minded individuals and create a formidable garrison for hopeful Zimbabweans.'},
       {name: 'developers', content: 'Zimbabweans'},
       {name: 'twitter:card', content: 'Converge 4 Development'},
